754c14578dfc9c36130d9baaca4a188fa3724f0f
Gandalf-Pl <529574002@qq.com>
2015-05-18 18:04:54 +0800

    add twisted test

---
+++ py_code/test_twisted.py
@@ -0,0 +1,17 @@
+# coding: utf8
+
+from twisted.web import server, resource
+from twisted.internet import reactor, endpoints
+
+
+class Counter(resource.Resource):
+    isLeaf = True
+    numberRequests = 0
+
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader("content-type", "text/plain")
+        return "I am request #" + str(self.numberRequests) + "\n"
+
+endpoints.serverFromString(reactor, "tcp:8080").listen(server.Site(Counter()))
+reactor.run()
